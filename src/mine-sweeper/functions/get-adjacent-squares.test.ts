9f9cf9c639c4a0d2a1024554e2eb71a293eddefa
JESShEart <jeshrt@protonmail.com>
2020-04-04 22:09:51 -0400

    created tests for get-status and get-adjacent-squares

---
+++ src/mine-sweeper/functions/get-adjacent-squares.test.ts
@@ -0,0 +1,97 @@
+import getAdjacentSquares from "./get-adjacent-squares";
+import { createTestBoard } from "./create-board.test";
+import Square from "../types/square";
+
+describe("getAdjacentSquares", function() {
+    let board: Square[][];
+
+    function verify(adjacentSquares: Square[], expectation: number[][]): void {
+        const expectedSquares: Square[][] = expectation.map((row, y) =>
+            row
+                .map((expected, x) => (expected ? board[y][x] : undefined))
+                .filter(it => it)
+        ) as Square[][];
+
+        expectedSquares
+            .reduce((it, n) => [...it, ...n], [])
+            .forEach(square => expect(adjacentSquares).toContain(square));
+    }
+
+    beforeEach(() => {
+        board = createTestBoard([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]);
+    });
+
+    test("should return adjacent squares on all sides", function() {
+        const adjacentSquares = getAdjacentSquares(board[1][1], board);
+        expect(adjacentSquares.length).toBe(8);
+        verify(adjacentSquares, [
+            [1, 1, 1],
+            [1, 0, 1],
+            [1, 1, 1]
+        ]);
+    });
+
+    test("should return adjacent squares on top, bottom and left", function() {
+        const adjacentSquares = getAdjacentSquares(board[1][2], board);
+        expect(adjacentSquares.length).toBe(5);
+        verify(adjacentSquares, [
+            [0, 1, 1],
+            [0, 1, 0],
+            [0, 1, 1]
+        ]);
+    });
+
+    test("should return adjacent squares on bottom and left", function() {
+        const adjacentSquares = getAdjacentSquares(board[0][2], board);
+        expect(adjacentSquares.length).toBe(3);
+        verify(adjacentSquares, [
+            [0, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]);
+    });
+
+    test("should return adjacent squares on top and right", function() {
+        const adjacentSquares = getAdjacentSquares(board[2][0], board);
+        expect(adjacentSquares.length).toBe(3);
+        verify(adjacentSquares, [
+            [0, 0, 0],
+            [1, 1, 0],
+            [0, 1, 0]
+        ]);
+    });
+
+    test("should return adjacent squares on top, right and left", function() {
+        const adjacentSquares = getAdjacentSquares(board[2][1], board);
+        expect(adjacentSquares.length).toBe(5);
+        verify(adjacentSquares, [
+            [0, 0, 0],
+            [1, 1, 1],
+            [1, 0, 1]
+        ]);
+    });
+
+    test("should return adjacent squares on top, right and bottom", function() {
+        const adjacentSquares = getAdjacentSquares(board[1][0], board);
+        expect(adjacentSquares.length).toBe(5);
+        verify(adjacentSquares, [
+            [1, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0]
+        ]);
+    });
+
+    test("should return adjacent squares on right, bottom and left", function() {
+        const adjacentSquares = getAdjacentSquares(board[0][1], board);
+        expect(adjacentSquares.length).toBe(5);
+        verify(adjacentSquares, [
+            [1, 0, 1],
+            [1, 1, 1],
+            [0, 0, 0]
+        ]);
+    });
+});
---
+++ src/mine-sweeper/functions/get-status.test.ts
@@ -0,0 +1,48 @@
+import getStatus from "./get-status";
+import Square from "../types/square";
+import { createTestBoard } from "./create-board.test";
+
+describe("getStatus", function() {
+    let board: Square[][];
+
+    function revealSquares(toReveal: number[][]): void {
+        toReveal.forEach((row, y) =>
+            row.forEach((revealed, x) => (board[y][x].revealed = !!revealed))
+        );
+    }
+
+    beforeEach(function() {
+        board = createTestBoard([
+            [0, 0, 0],
+            [0, 1, 0],
+            [1, 0, 0]
+        ]);
+    });
+
+    test("should be PLAY when 1 or more non-mines are not revealed", function() {
+        revealSquares([
+            [1, 1, 1],
+            [0, 0, 1],
+            [0, 0, 0]
+        ]);
+        expect(getStatus(board)).toBe("PLAY");
+    });
+
+    test("should be FAIL when at least one mine is revealed", function() {
+        revealSquares([
+            [0, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]);
+        expect(getStatus(board)).toBe("FAIL");
+    });
+
+    test("should be WIN when no mine is revealed and no un-revealed non-mine squares are found", function() {
+        revealSquares([
+            [1, 1, 1],
+            [1, 0, 1],
+            [0, 1, 1]
+        ]);
+        expect(getStatus(board)).toBe("WIN");
+    });
+});
