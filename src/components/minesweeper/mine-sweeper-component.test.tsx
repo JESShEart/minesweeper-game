8832723cf34df2f17a21b721f937c976ca836706
JESShEart <jeshrt@protonmail.com>
2020-04-19 17:25:44 -0400

    test for minesweeper-component

--- src/components/minesweeper/mine-sweeper-component.test.tsx
+++ src/components/minesweeper/mine-sweeper-component.test.tsx
@@ -1,5 +1,82 @@
-xdescribe("MineSweeperComponent", function() {
-    test("should test", function() {
-        fail();
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { shallow, ShallowWrapper } from "enzyme";
+import { ReactElement } from "react";
+import { h } from "preact";
+import { MineSweeperComponent } from "./mine-sweeper-component";
+import { Game } from "../../mine-sweeper/types/game";
+import { GameDispatch } from "../../mine-sweeper/game-reducer";
+import { StatsDispatch } from "../../stats/stats-reducer";
+import { minesweeperRouteProps } from "./minesweeper-route-props";
+import { BoardComponent } from "./board/board-component";
+import { StatusComponent } from "./status/status-component";
+import { ResetComponent } from "./reset/reset-component";
+import { TimerComponent } from "./timer/timer-component";
+import { FlaggingToggleComponent } from "./flagging-toggle/flagging-toggle-component";
+
+describe("MineSweeperComponent", function() {
+    let wrapper: ShallowWrapper;
+    let game: Game;
+    let title: string;
+    let gameDispatch: GameDispatch;
+    let statsDispatch: StatsDispatch;
+
+    beforeEach(function() {
+        game = {
+            status: "START",
+            startedAt: 1,
+            finishedAt: 2,
+            flagging: false
+        } as Game;
+        const titleUpdater = function(it: string): void {
+            title = it;
+        };
+        gameDispatch = function(): void {};
+        statsDispatch = function(): void {};
+        wrapper = shallow(
+            (
+                <MineSweeperComponent
+                    game={game}
+                    dispatch={gameDispatch}
+                    statsDispatch={statsDispatch}
+                    updateTitle={titleUpdater}
+                />
+            ) as ReactElement
+        );
+    });
+
+    test("should render status component", function() {
+        const statusComponent = wrapper.find(StatusComponent).props();
+        expect(statusComponent.status).toBe(game.status);
+    });
+
+    test("should render reset component", function() {
+        const resetComponent = wrapper.find(ResetComponent).props();
+        expect(resetComponent.dispatch).toBe(gameDispatch);
+    });
+
+    test("should render timer component", function() {
+        const timerComponent = wrapper.find(TimerComponent).props();
+        expect(timerComponent.startedAt).toBe(game.startedAt);
+        expect(timerComponent.finishedAt).toBe(game.finishedAt);
+    });
+
+    test("should render board component", function() {
+        const boardComponent = wrapper.find(BoardComponent).props();
+        expect(boardComponent.game).toBe(game);
+        expect(boardComponent.dispatch).toBe(gameDispatch);
+        expect(boardComponent.statsDispatch).toBe(statsDispatch);
+    });
+
+    test("should render flagging toggle component", function() {
+        const flaggingToggleComponent = wrapper
+            .find(FlaggingToggleComponent)
+            .props();
+        expect(flaggingToggleComponent.status).toBe(game.status);
+        expect(flaggingToggleComponent.flagging).toBe(game.flagging);
+        expect(flaggingToggleComponent.dispatch).toBe(gameDispatch);
+    });
+
+    test("should update title", function() {
+        expect(title).toBe(minesweeperRouteProps.title);
     });
 });
