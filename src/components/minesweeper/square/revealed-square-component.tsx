1d26c95306eed648439a539ff7db7a4c135f181e
JESShEart <jeshrt@protonmail.com>
2020-04-25 20:48:45 -0400

    more tweaks to accommodate the github site

--- src/components/minesweeper/square/revealed-square-component.tsx
+++ src/components/minesweeper/square/revealed-square-component.tsx
@@ -3,6 +3,7 @@ import { GameStatus } from "../../../minesweeper/types/game-status";
 import { Square } from "../../../minesweeper/types/square";
 import * as squareStyle from "./square-component.css";
 import * as style from "./revealed-square-component.css";
+import { minesweeperRouteProps } from "../minesweeper-route-props";
 
 interface Props {
     square: Square;
@@ -16,7 +17,8 @@ export function RevealedSquareComponent(props: Props): h.JSX.Element {
     function mineImg(): h.JSX.Element {
         const winStyle = status === "WIN" ? style.win : "";
         const className = ` ${winStyle} ${style.mine}`;
-        const mineImgSrc = "/assets/icons/mine.png";
+        const rootPath = minesweeperRouteProps.path;
+        const mineImgSrc = `${rootPath}/assets/icons/mine.png`;
         return <img className={className} src={mineImgSrc} alt="Mine" />;
     }
 
